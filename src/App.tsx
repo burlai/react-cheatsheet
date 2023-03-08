481593923208a43d8bed05734c406a6106d4edec
konstantyn-malczewski <konstantyn.malczewski@gmail.com>
2023-03-08 22:14:54 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1 className="h1">Hello, React Cheat Sheet!</h1>
+    </div>
+  );
+}
+
+export default App;
