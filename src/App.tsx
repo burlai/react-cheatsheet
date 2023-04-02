471c1f4d653039beff90a62ee32dd2a9a67ebc9c
konstantyn-malczewski <konstantyn.malczewski@gmail.com>
2023-04-02 13:58:55 +0200

    first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const Heading: React.FC = () => {
+  return (
+    <header>
+      <h1 className="h1">React Cheet Sheet</h1>
+      <h2 className="h2">Your own React pocket guide</h2>
+    </header>
+  );
+};
 
 const App: React.FC = () => {
-
   return (
     <div className="App">
-      <h1 className="h1">Hello, React Cheat Sheet!</h1>
+      <Heading />
     </div>
   );
-}
+};
 
 export default App;
